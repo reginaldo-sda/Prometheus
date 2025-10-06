dbb3b35582570abcffdccb47f4dfa27bae68137d
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2025-10-06 19:06:03 -0300

    Commit inicial prometheus (código já pronto)
    Código já tá pronto

---
+++ Prometheus.c
@@ -0,0 +1,115 @@
+/**
+  Projeto Finalizador da UC1: O Robô Prometheus
+  @authors Gabriel Alexandre, Felippe Camargo e Reginaldo Soares
+*/
+
+#include <DistanceSensor.h>
+
+constexpr int TrigPin = 13;
+constexpr int EchoPin = 12;
+
+DistanceSensor<TrigPin, EchoPin> sensor;
+
+bool girou;
+
+void setup() {
+  // Echo: pin12 / Trig: pin11
+  Serial.begin(9600);
+
+  // Pins dos sensores
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  // =================
+
+  // Pins dos motores
+  pinMode(5, OUTPUT);  // direito
+  pinMode(6, OUTPUT);  // direito
+
+  pinMode(9, OUTPUT);   // esquerdo
+  pinMode(10, OUTPUT);  // esquerdo
+  // =================
+
+  sensor.begin();
+}
+
+void loop() {
+  int distancia = sensor.tick();
+
+  if (distancia == sensor.NREADY) return;
+
+  Serial.print("Distancia do proximo obstaculo: ");
+  Serial.print(distancia);
+  Serial.println("cm");
+
+  // andarPraFrente();
+
+  if(distancia == -2) {
+    parar();
+  } else if (distancia > 12) {
+    andarPraFrente();
+  } else {
+    parar();
+    delay(1100);
+
+    andarPraEsquerda();
+    delay(350);
+  }
+}
+
+void andarPraFrente() {
+  analogWrite(5, 120);  // andar para trás
+  analogWrite(6, 0);    // andar para frente
+
+  analogWrite(9, 120);  // andar para frente
+  analogWrite(10, 0);   // andar para trás
+}
+
+void andarPraTras() {
+  analogWrite(5, 0);    // Andar para trás
+  analogWrite(6, 120);  // Andar para frente
+
+  analogWrite(9, 0);
+  analogWrite(10, 120);
+}
+
+void andarPraEsquerda() {
+  analogWrite(5, 115);  // Andar para trás
+  analogWrite(6, 0);    // Andar para frente
+
+  analogWrite(9, 0);
+  analogWrite(10, 100);
+}
+
+void andarPraDireita() {
+  analogWrite(5, 0);    // Andar para trás
+  analogWrite(6, 130);  // Andar para frente
+
+  analogWrite(9, 130);
+  analogWrite(10, 0);
+}
+
+void parar() {
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+}
+
+void movimentosDeTeste() {
+  andarPraFrente();
+
+  delay(1800);
+
+  andarPraTras();
+
+  delay(1800);
+
+  andarPraDireita();
+
+  delay(1200);
+
+  andarPraEsquerda();
+
+  delay(1200);
+}
